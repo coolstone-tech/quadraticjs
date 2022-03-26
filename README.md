6cbf0a495ec26661136769010593c8671528e019
System <69768251+systemsoftware69@users.noreply.github.com>
2022-03-25 18:38:38 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Quadratic.js
+Solve a Quadratic
+
+## Installation (via NPM)
+[`npm i quadratic.js`](https://docs.npmjs.com/downloading-and-installing-packages-locally/)
+## API
+```js
+const Quadratic = require('quadratic.js')
+const quadratic = new Quadratic(a,b,c)
+
+console.log(quadratic.solve()) // Solve quadratic
+console.log(quadratic.getDiscriminant()) // Get discriminant
+```
---
+++ WEB.js
@@ -0,0 +1,32 @@
+class Quadratic {
+
+    a = 0
+    b = 0
+    c = 0
+  
+    constructor(a = 1, b = -12, c = 32) {
+      if (a == 0) {
+        throw new Error("a cannot be 0")
+      }
+      this.a = a
+      this.b = b
+      this.c = c
+    }
+  
+    getDiscriminant() {
+      return (this.b ** 2 - (4 * this.a * this.c))
+    }
+  
+    solve() {
+  
+      if (this.getDiscriminant() < 0) {
+        return NaN
+      }
+      const root1 = ((-this.b + Math.sqrt(this.getDiscriminant())) / 2 * this.a)
+      const root2 = ((-this.b - Math.sqrt(this.getDiscriminant())) / 2 * this.a)
+  
+  
+      return [root1, root2]
+  
+    }
+  }
